3a9c1c26f969dd0a7e3d42de2b314e60347afb9a
Dumon <dumon.dmd@gmail.com>
2020-05-25 10:30:12 -0300

    Criando Login Admin

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
 
-	public function __contruct() {
+	public function __construct() {
 		$this->middleware('auth:admin');
 	}
 
--- routes/web.php
+++ routes/web.php
@@ -22,4 +22,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 
-Route::get('/admin', 'AdminController@index')->name('homeadmin');
+Route::get('/admin', 'AdminController@index')->name('admin.dashboard');
+
+Route::get('/admin/login', 'AdminController@index')->name('admin.login');
+
+Route::post('/admin/login', 'AdminController@index')->name('admin.login.submit');
